47e46c6838ea63f85a029684b786832742955064
Jamie Stewart <stwjme@gmail.com>
2020-06-09 17:36:13 -0400

    LocationsAdded

--- weather.js
+++ weather.js
@@ -1,17 +1,65 @@
 let express = require("express");
 let https = require("https");
+let bodyParser = require("body-parser");
 
 const weather = express();
+ 
+weather.use(bodyParser.urlencoded({extended: true}));
 
 weather.get("/", function(req, res){
-  let url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e72ca729af228beab5d20e3b7749713&units=metric"
+  res.sendFile(__dirname + "/weather.html");
+});
+//*****************Get Request*************************************//
+
+weather.post("/", function(req, res){
+  // console.log(req.body.cityName);
+  // console.log("Post request recieved.");
+  let query = req.body.cityName;
+  let apiKey = "e72ca729af228beab5d20e3b7749713";
+  let unit = "metric";
+  let url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey +"&units=" + unit;
+  // ***************Fetch Weahter Data From Extenal Server*********************************//
+  // **************Change the Weather Api from London*******************************//
+  //*****************Change apiKey ***********************************************//
 
   https.get(url, function(response){
-    console.log(response);
-  })
+    console.log(response.statusCode);
 
-  res.send("Server is up and running")
+    response.on ("data", function(data) {
+      let weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const iamgeURL = "http://openweathermap.org/img/wn/" + icon + "10d@2x.png"
+
+      res.write("<p>The weather is currently" + weatherDescription + "<p>");
+      res.write("<h1>The tempature in " + query + " is " + temp + "degrees Celcius.</h1>");
+      res.write("<img src=" + iamgeURL +">");
+      res.send()
+      //*************************Res reffers to our weather.get line 6 ***************/
+      //*************************Change The Temp From London **********************//
+
+      // console.log(weatherDescription);
+      // console.log(temp);
+
+
+      // let object = {
+      //   name: "Jamie",
+      //   favFood: "SeaFood"
+      // }
+      // console.log(data);
+      // console.log(JSON.stringify(object));
+
+    });
+  });
+
+  // res.send("Server is up and running")
+  // ***************Cant Have Multiple Send Request***********************//
+// })
+
+
 })
+
 
 
 
